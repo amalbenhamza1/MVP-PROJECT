f154c6616cd5220adeaa24bfbcfeac5ca7691420
amalbenhamza1 <benhamzaamal25@gmail.com>
2022-10-14 23:51:56 +0100

    still some work

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -18,12 +18,17 @@ const App = () => {
       author: author,
       createdAt:createdAt
     })
-      setBooksList([...booksList,{title: title,author: author,createdAt:createdAt}])
-    
+      setBooksList(
+        [...booksList,{title: title,author: author,createdAt:createdAt}]
+        )
+  }
+  const deleteBook = (id) => {
+    axios.delete("http://localhost:4000/api/books/:${id}",id);
   }
   return (
     <div className="App">
      <h1>BOOK FINDER APP</h1>
+     
      <div className='form'>
       <label>Tiltle:</label>
      <input type='text' name='title' onChange={(e)=>{
@@ -35,14 +40,26 @@ const App = () => {
      <input type='text' name='createdAt' onChange={(e)=>{
       setCreatedAt(e.target.value)}}/>
      <button onClick={addBook}>Add Book</button>
-     {booksList.map((val) => {
-      return <h3>Title: {val.title}
+     <div>
+      <h2>Books List:</h2>
+      {booksList.map((val) => {
+      return (
+        <center>
+          <div className='card'>
+            <h4>Title: {val.title}
       <br></br>
       Author: {val.author}
       <br></br>
-      Created At: {val.createdAt}</h3>     
+      Created At: {val.createdAt}</h4>
+      
+      </div><button onClick={()=>{deleteBook(val.id)}}>delete</button>
+      </center>
+        
+        )   
     })}
+    </div>
      </div>
+     
     </div>
   );
 }
